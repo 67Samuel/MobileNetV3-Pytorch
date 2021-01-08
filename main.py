9d238199770baf20804f3fe2dba983fda9755a0b
67Samuel <60543441+67Samuel@users.noreply.github.com>
2021-01-08 22:19:26 +0800

    Update main.py
    add SNIP pruning

--- main.py
+++ main.py
@@ -30,10 +30,132 @@ def get_args():
     parser.add_argument('--data', default='D:/ILSVRC/Data/CLS-LOC')
     parser.add_argument('--workers', type=int, default=4)
     parser.add_argument('--distributed', type=bool, default=False)
+    parser.add_argument('--prune', type=str, default=None)
+    parser.add_argument('--snip-percentage', type=int, default=0)
 
     args = parser.parse_args()
 
     return args
+
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+import copy
+import types
+
+def snip_forward_conv2d(self, x):
+        return F.conv2d(x, self.weight * self.weight_mask, self.bias,
+                        self.stride, self.padding, self.dilation, self.groups)
+
+
+def snip_forward_linear(self, x):
+        return F.linear(x, self.weight * self.weight_mask, self.bias)
+
+
+def SNIP(net, keep_ratio, train_dataloader, device, img_size=None, num_channels=3):
+    # TODO: shuffle?
+
+    # Grab a single batch from the training dataset
+    inputs, targets = next(iter(train_dataloader))
+    if type(img_size) == int:
+        inputs = inputs.view(-1,num_channels,img_size,img_size).float().requires_grad_()
+    inputs = inputs.to(device)
+    targets = targets.to(device)
+
+    # Let's create a fresh copy of the network so that we're not worried about
+    # affecting the actual training-phase
+    net = copy.deepcopy(net)
+
+    # Monkey-patch the Linear and Conv2d layer to learn the multiplicative mask
+    # instead of the weights
+    for layer in net.modules():
+        if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
+            layer.weight_mask = nn.Parameter(torch.ones_like(layer.weight))
+            nn.init.xavier_normal_(layer.weight)
+            layer.weight.requires_grad = False
+    
+        # Override the forward methods:
+        if isinstance(layer, nn.Conv2d):
+            layer.forward = types.MethodType(snip_forward_conv2d, layer)
+    
+        if isinstance(layer, nn.Linear):
+            layer.forward = types.MethodType(snip_forward_linear, layer)
+
+    # Compute gradients (but don't apply them)
+    net.zero_grad()
+    outputs = net.forward(inputs)
+    loss = nn.CrossEntropyLoss()(outputs, targets)
+    loss.backward()
+
+    grads_abs = []
+    for layer in net.modules():
+        if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
+            grads_abs.append(torch.abs(layer.weight_mask.grad))
+
+    # Gather all scores in a single vector and normalise
+    all_scores = torch.cat([torch.flatten(x) for x in grads_abs])
+    norm_factor = torch.sum(all_scores)
+    all_scores.div_(norm_factor)
+
+    num_params_to_keep = int(len(all_scores) * keep_ratio)
+    threshold, _ = torch.topk(all_scores, num_params_to_keep, sorted=True)
+    acceptable_score = threshold[-1]
+
+    keep_masks = []
+    for g in grads_abs:
+        keep_masks.append(((g / norm_factor) >= acceptable_score).float())
+
+    print(f"{torch.sum(torch.cat([torch.flatten(x == 1) for x in keep_masks]))} parameters kept out of {torch.sum(torch.cat([torch.flatten(x) for x in keep_masks]))}")
+
+    return(keep_masks)
+
+def apply_prune_mask(net, keep_masks):
+
+    # Before I can zip() layers and pruning masks I need to make sure they match
+    # one-to-one by removing all the irrelevant modules:
+    prunable_layers = filter(
+        lambda layer: isinstance(layer, nn.Conv2d) or isinstance(
+            layer, nn.Linear), net.modules())
+
+    for layer, keep_mask in zip(prunable_layers, keep_masks):
+        assert (layer.weight.shape == keep_mask.shape)
+
+        def hook_factory(keep_mask):
+            """
+            The hook function can't be defined directly here because of Python's
+            late binding which would result in all hooks getting the very last
+            mask! Getting it through another function forces early binding.
+            """
+
+            def hook(grads):
+                return grads * keep_mask
+            return hook
+
+        # mask[i] == 0 --> Prune parameter
+        # mask[i] == 1 --> Keep parameter
+
+        # Step 1: Set the masked weights to zero (NB the biases are ignored)
+        # Step 2: Make sure their gradients remain zero
+        layer.weight.data[keep_mask == 0.] = 0.
+        layer.weight.register_hook(hook_factory(keep_mask)) #hook masks onto respective weights
+  
+
+
+def initialize_xavier_normal(layer):
+	"""
+	Function to initialize a layer by picking weights from a xavier normal distribution
+	Arguments
+	---------
+	layer : The layer of the neural network
+	Returns
+	-------
+	None
+	"""
+	if type(layer) == nn.Conv2d:
+		torch.nn.init.xavier_normal_(layer.weight)
+		layer.bias.data.fill_(0)
 
 
 def adjust_learning_rate(optimizer, epoch, args):
@@ -41,6 +163,8 @@ def adjust_learning_rate(optimizer, epoch, args):
     lr = args.learning_rate * (0.1 ** (epoch // 30))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+        
+        
 
 
 # reference,
@@ -220,6 +344,13 @@ def main():
         print("init model load ...")
         epoch = 1
         best_acc1 = 0
+        
+    if args.prune:
+        print(f"Pruning {args.snip}% of weights with SNIP...")
+        # get snip factor in form required for SNIP function
+        snip_factor = (100 - args.snip_percentage)/100
+        keep_masks = SNIP(model, snip_factor, dataloader, device)
+        apply_prune_mask(model, keep_masks)
 
     optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, weight_decay=1e-5, momentum=0.9)
     # optimizer = optim.RMSprop(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=1e-5)
